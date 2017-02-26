9bbf12a46daa78dec6f8b6dc98a9be79f33622ec
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2017-02-26 22:32:06 +0100

    Stains!

--- app/src/main/java/com/campos/david/appointments/AboutActivity.java
+++ app/src/main/java/com/campos/david/appointments/AboutActivity.java
@@ -1,9 +1,13 @@
 package com.campos.david.appointments;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
@@ -17,18 +21,37 @@ import android.view.View;
 
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import java.util.ArrayList;
+
 public class AboutActivity extends AppCompatActivity {
     private static final String TAG = AboutActivity.class.getSimpleName();
 
     TextureView mTextureView = null;
     MyRenderer mRenderer = null;
 
+    Point mInitPos;
+    private static final String SAVED_POINT_Y = "point-y";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mTextureView = new TextureView(this);
-
         setContentView(mTextureView);
+        mInitPos = new Point(0, 0);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SAVED_POINT_Y, mRenderer.getProportionalCameraPos().y);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            mInitPos.y = savedInstanceState.getInt(SAVED_POINT_Y);
+        }
+        super.onRestoreInstanceState(savedInstanceState);
     }
 
     @Override
@@ -72,7 +95,7 @@ public class AboutActivity extends AppCompatActivity {
     }
 
 
-    private static class MyRenderer extends Thread implements TextureView.SurfaceTextureListener {
+    private class MyRenderer extends Thread implements TextureView.SurfaceTextureListener {
         public static final int STOPPED = 0;
         public static final int WAITING_TO_RUN = 1;
         public static final int RUNNING = 2;
@@ -90,14 +113,18 @@ public class AboutActivity extends AppCompatActivity {
 
         private int mBackgroundColor;
         private float mConstantDownCameraSpeed = 0.0f;
-        private float mCameraSpeed = 0.0f;
         private int mWidth = 0;
         private int mHeight = 0;
         private Paint mPaintTexts;
+        private Paint mPaintStains;
+        private Bitmap mStainBitmap;
+        private int mStainBitmapHalfWidth;
+        private int mStainBitmapHalfHeight;
         private Text[] mTexts;
+        private ArrayList<int[]> mStains;
         private Rect mCameraRect;
-        private Point mCameraDestiny = null;
-        private boolean mDoConstantDown = true;
+        private boolean mInitialized = false;
+        private float mSizeProportion;
 
         public void onResume() {
             synchronized (mSync) {
@@ -121,14 +148,12 @@ public class AboutActivity extends AppCompatActivity {
         }
 
         public void onScreenClicked(float x, float y) {
-            /*synchronized (mCameraLock) {
-                float centerX = mCameraRect.width() / 2.0f;
-                float centerY = mCameraRect.height() / 2.0f;
-                mCameraDestiny = new Point(
-                        Math.round(mCameraRect.left + x - centerX),
-                        Math.round(mCameraRect.top + y - centerY));
-                mDoConstantDown = false;
-            }*/
+            mStains.add(new int[]{
+                    Math.round(mCameraRect.left + x),
+                    Math.round(mCameraRect.top + y),
+                    (int) Math.round(Math.random() * 359), // Rotation
+                    (int) Math.round(Math.random() * 100) + 100 // Alpha
+            });
         }
 
         public MyRenderer(Context context) {
@@ -136,36 +161,50 @@ public class AboutActivity extends AppCompatActivity {
         }
 
         private void initialize() {
-            mBackgroundColor = mContext.getResources().getColor(R.color.colorPrimary);
-            mPaintTexts = new Paint(Paint.ANTI_ALIAS_FLAG);
-            mPaintTexts.setColor(mContext.getResources().getColor(R.color.textColorOverPrimary));
-            mPaintTexts.setTextAlign(Paint.Align.CENTER);
-            String licenseInfoStr = GoogleApiAvailability.getInstance().getOpenSourceSoftwareLicenseInfo(mContext);
-            String[] licenseInfo;
-            float sizeProportion = 6.0f;
-            if (licenseInfoStr != null) {
-                licenseInfo = licenseInfoStr.split("\n");
+            if (!mInitialized) {
+                mBackgroundColor = mContext.getResources().getColor(R.color.colorPrimary);
+                mPaintTexts = new Paint(Paint.ANTI_ALIAS_FLAG);
+                mPaintTexts.setColor(mContext.getResources().getColor(R.color.textColorOverPrimary));
+                mPaintTexts.setTextAlign(Paint.Align.CENTER);
+                mPaintStains = new Paint(Paint.ANTI_ALIAS_FLAG);
+                mPaintStains.setColorFilter(new PorterDuffColorFilter(
+                        mContext.getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP));
+                mStainBitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.stain);
+                mStainBitmapHalfWidth = mStainBitmap.getWidth() / 8; // We want to draw it at one quarter scale
+                mStainBitmapHalfHeight = mStainBitmap.getHeight() / 8;
+                mStains = new ArrayList<>();
+                String licenseInfoStr = GoogleApiAvailability.getInstance().getOpenSourceSoftwareLicenseInfo(mContext);
+                String[] licenseInfo;
+                mSizeProportion = 6.0f;
+                if (licenseInfoStr != null) {
+                    licenseInfo = licenseInfoStr.split("\n");
 
-                mTexts = new Text[licenseInfo.length + 4]; // Change also in else clause
-                int widest = 0;
-                for (String aLicenseInfo : licenseInfo) {
-                    if (aLicenseInfo.length() > widest)
-                        widest = aLicenseInfo.length();
+                    mTexts = new Text[licenseInfo.length + 4]; // Change also in else clause
+                    int widest = 0;
+                    for (String aLicenseInfo : licenseInfo) {
+                        if (aLicenseInfo.length() > widest)
+                            widest = aLicenseInfo.length();
+                    }
+                    mSizeProportion = (mWidth * 1.1f) / widest;
+                    for (int i = 0; i < licenseInfo.length; i++) {
+                        mTexts[i + 4] = new Text(0, Math.round((15 + 2 * i) * mSizeProportion), licenseInfo[i], 2 * mSizeProportion);
+                    }
+                } else {
+                    mTexts = new Text[4]; // Change also in if clause
                 }
-                sizeProportion = (mWidth * 1.1f) / widest;
-                for (int i = 0; i < licenseInfo.length; i++) {
-                    mTexts[i + 4] = new Text(0, Math.round((15 + 2 * i) * sizeProportion), licenseInfo[i], 2 * sizeProportion);
-                }
-            } else {
-                mTexts = new Text[4]; // Change also in if clause
+                mTexts[0] = new Text(0, 0, mContext.getString(R.string.app_name), 4 * mSizeProportion);
+                mTexts[1] = new Text(0, Math.round(4 * mSizeProportion),
+                        mContext.getString(R.string.text_author, mContext.getString(R.string.author)), 3 * mSizeProportion);
+                mTexts[2] = new Text(0, Math.round(7 * mSizeProportion),
+                        mContext.getString(R.string.author_mail), 3 * mSizeProportion);
+                mTexts[3] = new Text(0, Math.round(12 * mSizeProportion),
+                        mContext.getString(R.string.flags_attributions), 2 * mSizeProportion);
+
+                // Proportional original position of the camera
+                mCameraRect.top = Math.round(mCameraRect.top * mSizeProportion);
+
+                mInitialized = true;
             }
-            mTexts[0] = new Text(0, 0, mContext.getString(R.string.app_name), 4 * sizeProportion);
-            mTexts[1] = new Text(0, Math.round(4 * sizeProportion),
-                    mContext.getString(R.string.text_author, mContext.getString(R.string.author)), 3 * sizeProportion);
-            mTexts[2] = new Text(0, Math.round(7 * sizeProportion),
-                    mContext.getString(R.string.author_mail), 3 * sizeProportion);
-            mTexts[3] = new Text(0, Math.round(12 * sizeProportion),
-                    mContext.getString(R.string.flags_attributions), 2 * sizeProportion);
         }
 
         @Override
@@ -242,68 +281,62 @@ public class AboutActivity extends AppCompatActivity {
 
         private void updateCamera(float millisDelta) {
             synchronized (mCameraLock) {
-                if (mCameraDestiny != null) {
-                    Point distance = new Point(
-                            mCameraDestiny.x - mCameraRect.left,
-                            mCameraDestiny.y - mCameraRect.top);
-                    float speedLength = (float) Math.sqrt(distance.x * distance.x + distance.y * distance.y);
-                    Point speed = new Point(
-                            Math.round(distance.x / speedLength * mCameraSpeed * millisDelta),
-                            Math.round(distance.y / speedLength * mCameraSpeed * millisDelta));
-
-                    // Is speed in some direction more than needed?
-                    boolean inX = Math.abs(distance.x) <= Math.abs(speed.x);
-                    boolean inY = Math.abs(distance.y) <= Math.abs(speed.y);
-
-                    // Move...
-                    if (inX) {
-                        mCameraRect.offsetTo(mCameraDestiny.x, mCameraRect.top);
-                    } else {
-                        mCameraRect.offset(speed.x, 0);
-                    }
-                    if (inY) {
-                        mCameraRect.offsetTo(mCameraRect.left, mCameraDestiny.y);
-                    } else {
-                        mCameraRect.offset(0, speed.y);
-                    }
-
-                    if (inX && inY) {
-                        mCameraDestiny = null;
-                    }
-                } else if (mDoConstantDown) {
-                    mCameraRect.offset(0, Math.round(mConstantDownCameraSpeed * millisDelta));
-                }
+                mCameraRect.offset(0, Math.round(mConstantDownCameraSpeed * millisDelta));
             }
+        }
+
+        public Point getProportionalCameraPos() {
+            return new Point(
+                    Math.round(mCameraRect.left / mSizeProportion),
+                    Math.round(mCameraRect.top / mSizeProportion));
         }
 
         private void drawFrame(Canvas canvas) {
             canvas.drawColor(mBackgroundColor);
             synchronized (mCameraLock) {
+                int drawnTexts = 0;
                 for (Text text : mTexts) {
                     if (text.text != null) {
                         mPaintTexts.setTextSize(text.size);
                         Point pos = toCameraCoordinates(text.posX, text.posY);
                         float margin = 1.1f * text.size;
                         if (pos.y > -margin && pos.y < mHeight + margin) {
+                            drawnTexts++;
                             canvas.drawText(text.text, pos.x, pos.y, mPaintTexts);
                         }
                     }
                 }
-                // For debug
-//                mPaintTexts.setTextSize(10);
-//                Paint.Align oldAlign = mPaintTexts.getTextAlign();
-//                mPaintTexts.setTextAlign(Paint.Align.LEFT);
-//                String info0, info1;
-//                info0 = "Cam(" + mCameraRect.left + ", " + mCameraRect.top + "; " +
-//                        mCameraRect.width() + ", " + mCameraRect.height() + ")";
-//                if(mCameraDestiny != null) {
-//                     info1 = "Dst(" + mCameraDestiny.x + ", " + mCameraDestiny.y + ")";
-//                } else {
-//                    info1 = "Dst(no, no)";
-//                }
-//                canvas.drawText(info0, 0, 40, mPaintTexts);
-//                canvas.drawText(info1, 0, 50, mPaintTexts);
-//                mPaintTexts.setTextAlign(oldAlign);
+
+                if (drawnTexts == 0) {
+                    // If not a single text has been drawn, move to the start
+                    mCameraRect.offsetTo(-mWidth / 2, -mHeight / 2);
+                    mStains.clear();
+                } else {
+                    ArrayList<Integer> toRemove = new ArrayList<>();
+                    for (int i = 0; i < mStains.size(); i++) {
+                        int[] stain = mStains.get(i);
+                        Rect stainRect = new Rect(
+                                stain[0], stain[1],
+                                stain[0] + 2 * mStainBitmapHalfWidth, stain[1] + 2 * mStainBitmapHalfHeight);
+                        if (stainRect.intersect(mCameraRect)) {
+                            Point pos = toCameraCoordinates(stain[0], stain[1]);
+                            canvas.translate(pos.x, pos.y);
+                            canvas.rotate(stain[2]);
+                            Rect draw = new Rect(-mStainBitmapHalfWidth, -mStainBitmapHalfHeight,
+                                    mStainBitmapHalfWidth, mStainBitmapHalfHeight);
+                            mPaintStains.setAlpha(stain[3]);
+                            canvas.drawBitmap(mStainBitmap, null, draw, mPaintStains);
+                            canvas.rotate(-stain[2]);
+                            canvas.translate(-pos.x, -pos.y);
+                        } else {
+                            // If it is out of screen remove it
+                            toRemove.add(i);
+                        }
+                    }
+                    for (int i = 0; i < toRemove.size(); i++) {
+                        mStains.remove((int) toRemove.get(i));
+                    }
+                }
             }
         }
 
@@ -320,9 +353,9 @@ public class AboutActivity extends AppCompatActivity {
                 mSurface = surface;
                 synchronized (mCameraLock) {
                     mConstantDownCameraSpeed = height / 20000.0f; // One height each twenty seconds
-                    mCameraSpeed = width / 1000.0f; // One complete width each second
-                    int x = -width / 2;
-                    int y = -height / 2;
+                    int x = mInitPos.x - width / 2;
+                    int y = mInitPos.y - height / 2;
+                    Log.d(TAG, "Initial position " + x + "," + y);
                     mWidth = width;
                     mHeight = height;
                     mCameraRect = new Rect(x, y, x + width, y + height);
@@ -335,7 +368,6 @@ public class AboutActivity extends AppCompatActivity {
         public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
             synchronized (mCameraLock) {
                 mConstantDownCameraSpeed = height / 20000.0f; // One height each twenty seconds
-                mCameraSpeed = width / 1000.0f; // One complete width each second
                 mWidth = width;
                 mHeight = height;
                 mCameraRect.set(mCameraRect.left, mCameraRect.top,
